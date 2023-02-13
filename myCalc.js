b92ba2e203dcbac559e2db25cfe3e0132ccdfcb0
Fleurie <123471150+angelineza@users.noreply.github.com>
2023-02-13 14:58:44 +0200

    Add files via upload

---
+++ myCalc.js
@@ -0,0 +1,14 @@
+function add(nums){
+            let sum=0;
+            for(let i=0; i<nums.length; i++){
+                sum+=nums[i];
+            };
+            console.log('The sum of the numbers is'+sum);
+        };
+function multiply(nums){
+     let product=1;
+      for(let i=0; i<nums.length; i++){
+           product*=nums[i];
+            };
+    console.log('The product of the numbers is'+product);
+};
